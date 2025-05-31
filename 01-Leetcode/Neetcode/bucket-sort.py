98951b0473bb0449cc0993e87fee68458f828206
spdy999 <1a1a1a.262626.0010@gmail.com>
2025-05-31 20:48:36 +0700

    vault backup: 2025-05-31 20:48:36, 3
    Affected files:
    01-Leetcode/Neetcode/bucket-sort.py
    01-Leetcode/Neetcode/dijkstra-algorithm.py
    01-Leetcode/Neetcode/knapsack.py

--- 01-Leetcode/Neetcode/bucket-sort.py
+++ 01-Leetcode/Neetcode/bucket-sort.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 # Check leetcode version. This version can be used only consecutive order
-arr = [2,1,2,0,0,2]
+arr = [2, 1, 2, 0, 0, 2]
 # arr = [2,0]
 counter = Counter(arr)
 print(arr)
@@ -11,6 +11,6 @@ i = 0
 for n in range(len(counter)):
     for _ in range(counter[n]):
         arr[i] = n
-        print('i:', i, 'n:', n, arr)
+        print("i:", i, "n:", n, arr)
         i += 1
 print(arr)
