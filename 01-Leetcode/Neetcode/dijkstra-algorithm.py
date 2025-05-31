4cd31979c07f9b99c8d0ec59fa6636fa9d194db8
spdy999 <1a1a1a.262626.0010@gmail.com>
2025-05-31 19:48:13 +0700

    vault backup: 2025-05-31 19:48:12, 1
    Affected files:
    01-Leetcode/Neetcode/dijkstra-algorithm.py

--- 01-Leetcode/Neetcode/dijkstra-algorithm.py
+++ 01-Leetcode/Neetcode/dijkstra-algorithm.py
@@ -1,7 +1,12 @@
+from typing import List, Dict
+import collections
+import heapq
+
+
 class Solution:
     def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
         ed = collections.defaultdict(list)
-        t = dict() # { node: shortest distance }
+        t = dict()  # { node: shortest distance }
         for i in range(n):
             t[i] = -1
 
@@ -9,10 +14,10 @@ class Solution:
             ed[u].append((v, w))
         minHeap = [(0, src)]
         visit = set()
-        
+
         while minHeap:
             w1, n1 = heapq.heappop(minHeap)
-            
+
             if n1 in visit:
                 continue
             visit.add(n1)
@@ -21,7 +26,20 @@ class Solution:
             for n2, w2 in ed[n1]:
                 if n2 not in visit:
                     heapq.heappush(minHeap, (w1 + w2, n2))
-                
+
         return t
 
-print(Solution.shortestPath(3, [[0,1,4]], 0))
+
+def test():
+    solution = Solution()
+    assert solution.shortestPath(3, [[0, 1, 4]], 0) == dict({0: 0, 1: 4, 2: -1})
+    assert solution.shortestPath(
+        5,
+        [[0, 1, 10], [0, 2, 3], [1, 3, 2], [2, 1, 4], [2, 3, 8], [2, 4, 2], [3, 4, 5]],
+        0,
+    ) == dict({0: 0, 1: 7, 2: 3, 3: 9, 4: 5})
+
+
+if __name__ == "__main__":
+    test()
+    print("Correct Dijkstra's shortest path")
