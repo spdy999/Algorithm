881e8f9f60badf7da87ce6aed1c209f3317e2d4a
spdy999 <1a1a1a.262626.0010@gmail.com>
2025-06-04 00:05:30 +0700

    vault backup: 2025-06-04 00:05:30, 6
    Affected files:
    01-Leetcode/1514. Path with Maximum Probability.md
    01-Leetcode/1514.path-with-maximum-probability.py
    01-Leetcode/778. Swim in Rising Water 2025-06-03 22.25.27.excalidraw.md
    01-Leetcode/778. Swim in Rising Water.md
    01-Leetcode/778.swim-in-rising-water.py
    01-Leetcode/@IND_01-Leetcode.md

---
+++ 01-Leetcode/1514.path-with-maximum-probability.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=1514 lang=python3
+#
+# [1514] Path with Maximum Probability
+#
+
+# @lc code=start
+class Solution:
+    def maxProbability(
+        self,
+        n: int,
+        edges: List[List[int]],
+        succProb: List[float],
+        start_node: int,
+        end_node: int,
+    ) -> float:
+        ed = defaultdict(list)
+        prob = defaultdict(int)  # {node: maxProb}
+
+        for uv, w in zip(edges, succProb):
+            u, v = uv
+            ed[u].append((v, w))
+            ed[v].append((u, w))
+
+        maxHeap = [(-1, start_node)]
+        visit = set()
+
+        while maxHeap:
+            w1, n1 = heapq.heappop(maxHeap)
+            w1 = w1
+
+            if n1 in visit:
+                continue
+
+            visit.add(n1)
+            prob[n1] = min(prob[n1], w1)
+
+            for n2, w2 in ed[n1]:
+                if n2 not in visit:
+                    heapq.heappush(maxHeap, (w1 * w2, n2))
+        return -prob[end_node]
+
+        
+# @lc code=end
+
