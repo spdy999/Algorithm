979dfee3b0477e79e437896ea33b021f2535adf3
spdy999 <1a1a1a.262626.0010@gmail.com>
2025-09-17 01:59:47 +0700

    vault backup: 2025-09-17 01:59:47, 16
    Affected files:
    01-Leetcode/1462. Course Schedule IV (conflict 2025-09-17-00-57-13).md
    01-Leetcode/1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree (conflict 2025-09-17-00-57-13).md
    01-Leetcode/1584. Min Cost to Connect All Points (conflict 2025-09-17-00-57-14).md
    01-Leetcode/206. Reverse Linked List 2024-05-24 19.16.58.excalidraw (conflict 2025-09-17-00-57-14).md
    01-Leetcode/207. Course Schedule (conflict 2025-09-17-00-57-14).md
    01-Leetcode/207. Course Schedule 2025-07-04 16.17.53.excalidraw (conflict 2025-09-17-00-57-14).md
    01-Leetcode/210. Course Schedule II (conflict 2025-09-17-00-57-14).md
    01-Leetcode/729. My Calendar I (conflict 2025-09-17-00-57-14).md
    01-Leetcode/729.my-calendar-i (conflict 2025-09-17-00-57-14).py
    01-Leetcode/743. Network Delay Time (conflict 2025-09-17-00-57-14).md
    01-Leetcode/743. Network Delay Time 2025-05-30 00.58.03.excalidraw (conflict 2025-09-17-00-57-14).md
    01-Leetcode/743.network-delay-time (conflict 2025-09-17-00-57-14).py
    01-Leetcode/75. Sort Colors (conflict 2025-09-17-00-57-15).md
    01-Leetcode/75.sort-colors (conflict 2025-09-17-00-57-15).py
    01-Leetcode/@IND_01-Leetcode.md
    @IND_OBS-Algorithm.md

---
+++ 01-Leetcode/75.sort-colors (conflict 2025-09-17-00-57-15).py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Bucket sort
+        bucket = Counter(nums)
+        maxx = max(nums) + 1
+        j = 0
+        for i in range(maxx):
+            if i in bucket:
+                for _ in range(bucket[i]):
+                    nums[j] = i
+                    j += 1
+        return nums
+
+
+assert Solution().sortColors([2, 0, 2, 1, 1, 0]) == [0, 0, 1, 1, 2, 2]
+assert Solution().sortColors([2, 0, 1]) == [0, 1, 2]
+
+# @lc code=end
