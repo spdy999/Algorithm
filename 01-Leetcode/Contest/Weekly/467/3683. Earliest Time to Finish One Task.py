95723fba8fd8b12b6e049803472abaa6f658c81c
spdy999 <1a1a1a.262626.0010@gmail.com>
2025-09-22 09:10:50 +0700

    vault backup: 2025-09-22 09:10:50, 8
    Affected files:
    01-Leetcode/678.valid-parenthesis-string.py
    01-Leetcode/Contest/Weekly/467/3683. Earliest Time to Finish One Task 2025-09-20 12.50.34.excalidraw.md
    01-Leetcode/Contest/Weekly/467/3683. Earliest Time to Finish One Task.md
    01-Leetcode/Contest/Weekly/467/3683. Earliest Time to Finish One Task.py
    01-Leetcode/Contest/Weekly/467/3684. Maximize Sum of At Most K Distinct Elements.md
    01-Leetcode/Contest/Weekly/467/3684. Maximize Sum of At Most K Distinct Elements.py
    01-Leetcode/Contest/Weekly/467/@IND_467.md
    01-Leetcode/Contest/Weekly/@IND_Weekly.md

--- 01-Leetcode/Contest/Weekly/467/3683. Earliest Time to Finish One Task.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def earliestTime(self, tasks: List[List[int]]) -> int:
-        minn = float('inf')
-        for s, t in tasks:
-            minn = min(minn, s + t)
-        return minn
--- 01-Leetcode/Contest/Weekly/467/3684. Maximize Sum of At Most K Distinct Elements.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
-        nums = list(set(nums))
-        nums.sort(reverse=True)
-        return nums[:k]
-      
